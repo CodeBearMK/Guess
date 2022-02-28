8cad13b528433a3b271867b7c383b65ed066964c
cat32tw <mike1993xd@gmail.com>
2022-02-28 22:08:47 +0800

    猜數字遊戲

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Guess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblSmall = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblLarge = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // lblSmall
+            // 
+            this.lblSmall.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblSmall.Location = new System.Drawing.Point(32, 44);
+            this.lblSmall.Name = "lblSmall";
+            this.lblSmall.Size = new System.Drawing.Size(62, 54);
+            this.lblSmall.TabIndex = 0;
+            this.lblSmall.Text = "10";
+            this.lblSmall.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(100, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 54);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "＜";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(159, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 54);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "？";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(227, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 54);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "＜";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblLarge
+            // 
+            this.lblLarge.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblLarge.Location = new System.Drawing.Point(295, 45);
+            this.lblLarge.Name = "lblLarge";
+            this.lblLarge.Size = new System.Drawing.Size(62, 54);
+            this.lblLarge.TabIndex = 0;
+            this.lblLarge.Text = "99";
+            this.lblLarge.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "請猜出中間的數字：";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(261, 130);
+            this.txtGuess.Multiline = true;
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(100, 31);
+            this.txtGuess.TabIndex = 1;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(36, 196);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(84, 33);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "確定";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(163, 196);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(86, 33);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "重玩";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(282, 196);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(79, 33);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "離開";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 257);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.lblLarge);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSmall);
+            this.Name = "Form1";
+            this.Text = "猜數字遊戲";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSmall;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblLarge;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guess
+{
+    public partial class Form1 : Form
+    {
+        int largeN, smallN, ansN, guessN;
+
+        private void getStart()
+        {
+            largeN = 99;
+            smallN = 10;
+            Random rnd = new Random();
+            ansN = rnd.Next(11, 99);
+            lblLarge.Text = largeN.ToString();
+            lblSmall.Text = smallN.ToString();
+            clsGuess();
+        }
+
+        private void clsGuess()
+        {
+            txtGuess.Clear();
+            txtGuess.Focus();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            getStart();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(this.txtGuess, "請輸入一個二位數數值！");
+            getStart();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                guessN = int.Parse(txtGuess.Text);
+            }
+            catch
+            {
+                MessageBox.Show("請輸入一個二位數數值！", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                clsGuess();
+                return;
+            }
+
+            if (guessN == ansN)
+            {
+                MessageBox.Show("恭喜你答對！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                if (guessN > largeN || guessN < smallN)
+                {
+                    string msg = "請輸入介於 " + smallN + " 與 " + largeN + " 之間的數值";
+                    MessageBox.Show(msg, "警告", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                else if (guessN > ansN)
+                {
+                    MessageBox.Show("輸入小一點的數", "提示", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    largeN = guessN;
+                    lblLarge.Text = largeN.ToString();
+                }
+                else if (guessN < ansN)
+                {
+                    MessageBox.Show("輸入大一點的數", "提示", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    smallN = guessN;
+                    lblSmall.Text = smallN.ToString();
+                }
+                clsGuess();
+            }
+        }
+    }
+}
